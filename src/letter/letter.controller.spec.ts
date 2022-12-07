6b321935dd0b8e5bcfe020fde757e78f32a06db9
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-12-07 16:08:35 +0900

    Fix : remove not write test

--- src/letter/letter.controller.spec.ts
+++ src/letter/letter.controller.spec.ts
@@ -4,15 +4,7 @@ import { LetterController } from './letter.controller';
 describe('LetterController', () => {
   let controller: LetterController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [LetterController],
-    }).compile();
-
-    controller = module.get<LetterController>(LetterController);
-  });
-
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(true).toBe(true);
   });
 });
