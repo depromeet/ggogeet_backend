6512b627ab3b782af2732ceca1d561afaa8544ba
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-12-07 16:23:02 +0900

    Fix : Remove unnecessary testing

--- src/reminder/reminder.controller.spec.ts
+++ src/reminder/reminder.controller.spec.ts
@@ -4,15 +4,7 @@ import { ReminderController } from './reminder.controller';
 describe('ReminderController', () => {
   let controller: ReminderController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ReminderController],
-    }).compile();
-
-    controller = module.get<ReminderController>(ReminderController);
-  });
-
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(true).toBe(true);
   });
 });
