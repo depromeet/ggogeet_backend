755ecf237280d73b20e01b8426a56ac5d6f1716b
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-12-22 22:38:21 +0900

    Feat: Add Swagger

---
+++ src/utils/swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('ggo-geet API Docs')
+    .setDescription('ggo-geet  API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
