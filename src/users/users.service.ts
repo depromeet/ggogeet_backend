706f138f3c677212552abf8f0f62a765b021cd2b
Ryuhyis <skyblue22756@gmail.com>
2022-12-23 18:37:11 +0900

    Feat:  Add Find user by id api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateKakaoUserDto } from 'src/auth/dto/requests/createKakaoUser.dto';
-import { UpdateUserDto } from './dto/requests/updateUser.dto';
+import { Repository } from 'typeorm';
+import { UserResponseDto } from './dto/response/user.response.dto';
+import { Social } from './entities/social.entity';
+import { User } from './entities/user.entity';
+import { UserInfo } from './entities/userinfo.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateKakaoUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Social) private socialRepository: Repository<Social>,
+    @InjectRepository(UserInfo)
+    private userInfoRepository: Repository<UserInfo>,
+  ) {}
+
+  async findUserById(id) {
+    const user = await this.userRepository.findOne({
+      where: { id: id },
+      relations: { userInfo: true, social: true },
+      select: ['id', 'name', 'profileImg', 'social', 'userInfo'],
+    });
+    return new UserResponseDto(user);
   }
 
   findAll() {
@@ -14,10 +31,6 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
