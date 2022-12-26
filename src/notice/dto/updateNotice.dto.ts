16dfe704616a97ab859a6b0f6ac7fb0c558851f8
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-12-26 17:09:12 +0900

    Refactor : change PartialType import

--- src/notice/dto/updateNotice.dto.ts
+++ src/notice/dto/updateNotice.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateNoticeDto } from './createNotice.dto';
 
 export class UpdateNoticeDto extends PartialType(CreateNoticeDto) {}
--- src/reminder/dto/updateReminder.dto.ts
+++ src/reminder/dto/updateReminder.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateReminderDto } from './createReminder.dto';
 
 export class UpdateReminderDto extends PartialType(CreateReminderDto) {}
