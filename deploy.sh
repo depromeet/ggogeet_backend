f345fd41c4dc096e0332c338d53f7eaa67ee49d2
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-01-08 19:10:25 +0900

    Feat : change to docker compose

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 
 cd ~/ggogeet_backend
 git pull origin master
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker build -t ggogeet_backend .
-sudo docker run -d -p 3000:3000 ggogeet_backend
+sudo docker-compose up --build -d
