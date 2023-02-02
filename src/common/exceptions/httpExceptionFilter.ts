9c9f2c1f6977d4da9183677dd592fdbbbe892e13
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-02-02 19:21:36 +0700

    Feat : internal server error logging and alert (#102)
    * Fix : fix internal server error logging
    
    * Feat : add telegram monitoring to 500 error

--- src/common/exceptions/httpExceptionFilter.ts
+++ src/common/exceptions/httpExceptionFilter.ts
@@ -4,10 +4,10 @@ import {
   ExceptionFilter,
   HttpException,
   Inject,
-  InternalServerErrorException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { WINSTON_MODULE_PROVIDER, WinstonLogger } from 'nest-winston';
+import { TelegramMonitoringService } from 'src/monitoring/monitoring.telegram';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -19,18 +19,26 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
+    const errorTime = new Date().toLocaleString();
+    const monitoring = new TelegramMonitoringService();
 
-    if (!(exception instanceof HttpException)) {
-      exception = new InternalServerErrorException();
-      this.logger.error(
-        `[${req.method}] ${req.url} ${res.statusCode} - ${exception}}`,
-      );
+    if (!(exception instanceof HttpException) || res.statusCode >= 500) {
+      const errorMsg = `[${req.method}] ${req.url} ${res.statusCode} - ${exception} - ${errorTime}`;
+      monitoring.sendAlert(errorMsg);
+      this.logger.error(errorMsg);
+
+      return res.status(500).json({
+        statusCode: 500,
+        message: 'Internal Server Error',
+        error: 'Internal Server Error',
+      });
     }
 
     const response = (exception as HttpException).getResponse();
     const status = (exception as HttpException).getStatus();
 
-    this.logger.error(`[${req.method}] ${req.url} ${status} - ${exception}}`);
+    const httpErrorMsg = `[${req.method}] ${req.url} ${status} - ${exception} - ${errorTime}`;
+    this.logger.error(httpErrorMsg);
 
     res.status((exception as HttpException).getStatus()).json(response);
   }
---
+++ src/monitoring/monitoring.telegram.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TelegramMonitoringService {
+  private chatId: string;
+  private botToken: string;
+
+  constructor() {
+    this.chatId = process.env.TELEGRAM_CHAT_ID;
+    this.botToken = process.env.TELEGRAM_BOT_TOKEN;
+  }
+
+  public sendAlert(message: string) {
+    const config = {
+      method: 'get',
+      url: `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${this.chatId}&text=${message}`,
+      headers: {},
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
