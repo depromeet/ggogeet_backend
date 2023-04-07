b1ca44be81e9aae6968dd44862bdb8172b9fe749
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-07 22:30:05 +0900

    Refactor: change infra to infra dir

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './utils/swagger';
+import { setupSwagger } from './infrastructor/document/document.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
